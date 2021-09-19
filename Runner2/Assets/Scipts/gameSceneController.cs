ca4d988fb046d593710b2a24f2564df6d4a63d75
christoffel <c.toffel9@gmail.com>
2021-09-19 21:38:16 +0700

    Update

---
+++ Runner2/Assets/Scipts/gameSceneController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+public class gameSceneController : MonoBehaviour
+{
+    public playerController player;
+    public Camera maincamera;
+    public Text gameText;
+    public Text gameOver;
+    public GameObject[] blockPrefabs;
+    private float blockPointer = -10;
+    private float safeMargin = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (player != null && blockPointer < player.transform.position.x + safeMargin)
+        {
+            int blockIndex = Random.Range(0, blockPrefabs.Length);
+            if (blockPointer < 20)
+            {
+                blockIndex = 0;
+            }
+            GameObject blockObject = GameObject.Instantiate<GameObject>(blockPrefabs[blockIndex]);
+            blockObject.transform.SetParent(this.transform);
+            blockController block = blockObject.GetComponent<blockController>();
+            blockObject.transform.position = new Vector3(blockPointer + block.size / 2, 0, 0);
+            blockPointer += block.size;
+        }
+        if (player != null)
+        {
+            maincamera.transform.position = new Vector3(
+                player.transform.position.x,
+                maincamera.transform.position.y,
+                maincamera.transform.position.z);
+
+            gameText.text = "Score :" + Mathf.Floor(player.transform.position.x);
+        } else if (player == null)
+        {
+            gameText.text = "\n Press R to Restart";
+            if (Input.GetKeyDown("r"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        }
+        
+
+
+    }
+
+   
+}
---
+++ Runner2/Assets/Scipts/playerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerController : MonoBehaviour
+{
+    public GameObject player;
+    public float jumpforce;
+    public Rigidbody rb;
+    public int jump;
+    public float horizontalVelocity;
+    public int jumps;
+       // Start is called before the first frame update
+    void Start()
+    {
+        jumps = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(horizontalVelocity * Time.deltaTime, rb.velocity.y, rb.velocity.z);
+
+        if (Input.GetKeyDown("space") && jumps > 0)
+        {
+            rb.AddForce(new Vector3(0, jumpforce, 0));
+            jumps = jumps - 1;
+        }
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.GetComponent<obstacleController>() != null)
+        {
+            GameObject.Destroy(this.gameObject);
+
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "ground")
+        {
+            Debug.Log("nyentuhGround");
+            jumps = jumps + 1;
+        }
+    }
+
+
+}
