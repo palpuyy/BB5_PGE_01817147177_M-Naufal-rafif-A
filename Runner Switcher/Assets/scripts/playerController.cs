20b84a5c2e1161436c6eb5c1bac95c3f97e71d6e
Chrsitoffel <c.toffel9@gmail.com>
2021-10-10 13:46:28 +0700

    Runner Switcher

---
+++ Runner Switcher/Assets/scripts/playerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public int score;
+    public float moveForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Physics.gravity.y > 0)
+        {
+            Physics.gravity *= -1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Physics.gravity *= -1;
+        }
+        this.GetComponent<Rigidbody>().velocity = new Vector3(moveForce*Time.deltaTime, this.GetComponent<Rigidbody>().velocity.y, this.GetComponent<Rigidbody>().velocity.z);
+
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("obstacle"))
+        {
+            Destroy(gameObject);
+        }
+        if (other.gameObject.CompareTag("coin"))
+        {
+            Destroy(other.gameObject);
+            score = score + 10;
+        }
+    }
+}
